adbeb6d4c395993d7362ba3611468423d3521f9f
mohamadou moctar <moctarm99@gmail.com>
2021-03-01 09:56:25 +0600

    fix router refresh

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Main from './components/MainComponent';
-import { BrowserRouter} from 'react-router-dom';
+import { BrowserRouter, HashRouter} from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { ConfigureStore } from './redux/ConfigureStore';
 
@@ -11,11 +11,11 @@ class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <BrowserRouter>
+        <HashRouter>
           <div className="App">
             <Main />
           </div>
-        </BrowserRouter>
+        </HashRouter>
       </Provider>
     );
   }
